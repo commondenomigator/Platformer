650b99db6f17aa9d4f72ec57db57113b69071d52
Nick Wissman <nickwissman@gmail.com>
2022-03-13 10:48:22 -0700

    part 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,23 +6,36 @@ public class PlayerController : MonoBehaviour
 {
 
     Rigidbody2D RB;
+    CapsuleCollider2D CC;
     public float Speed = 5f;
     public float JumpPower = 8f;
+    public Vector2 StartPosition = new Vector2(0, 0);
+    public float Border = -10f;
 
     // Start is called before the first frame update
     void Start()
     {
         RB = GetComponent<Rigidbody2D>();
+        CC = GetComponent<CapsuleCollider2D>();
+        Reset();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump")) {
+        if (Input.GetButtonDown("Jump") && CC.IsTouchingLayers()) {
             Debug.Log("Jump!");
             RB.velocity = new Vector2(0, JumpPower);
         }
 
         RB.velocity = new Vector2(Input.GetAxis("Horizontal") * Speed, RB.velocity.y);
+        if (transform.position.y < Border) {
+            Reset();
+        }
+    }
+
+    void Reset() {
+        transform.position = StartPosition;
+        RB.velocity = Vector2.zero;
     }
 }
